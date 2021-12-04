e8acb927391fb42ee77569a080f49a34649497c5
Ali El Husseini <alielhusseini@Alis-MacBook-Air.local>
2021-12-04 22:03:17 +0200

    starting with ts

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var character = 'Ali El Husseini';
+var inputs = document.querySelectorAll('input');
+console.log(character);
+inputs.forEach(function (input) { return console.log(input); });
---
+++ sandbox.ts
@@ -0,0 +1,5 @@
+const character = 'Ali El Husseini'
+const inputs = document.querySelectorAll('input')
+
+console.log(character)
+inputs.forEach(input => console.log(input))
